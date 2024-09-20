713332694eb8fc3b409bd24507acdc7c9906b829
Bhargavi576 <bhargavidasari576@gmail.com>
2024-09-20 16:07:23 +0530

    intro to py

--- project/project_scraping.py
+++ project/project_scraping.py
@@ -1,20 +1,12 @@
-import requests
+import requests 
 from bs4 import BeautifulSoup
-
-
-url = 'https://cabs.toscrape.com/'
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
-headings = soup.find_all('h3')
-
-
-
-cabs_info = []
-for heading in headings:
-    cab_title = heading.find('a')['title']
-    cabs_info.append(cab_title)
-
-
-with open('cabs_info.txt', 'w', encoding='UTF-8') as cab_file:
-    for cab in cabs_info:
-        cab_file.write(cab + '\n')
+url = 'https://www.olacabs.com'
+news_res = requests.get(url)
+soup = BeautifulSoup(news_res.content, 'html.parser')
+headings = soup.find_all('h2')
+with open('news_headings.txt','w',encoding='UTF-8') as news_file:
+    for heading in headings:
+        news_file.write(heading.text + '\n')
+    #endfor
+#endwith
+print('ola cabs gathered')
